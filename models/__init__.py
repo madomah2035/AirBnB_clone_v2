b886b1eebcc90237d816d2910584c232190fb16d
Musah ADOMAH <134120429+madomah2035@users.noreply.github.com>
2024-03-23 23:48:03 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -17,10 +17,3 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
 else:
     storage = FileStorage()
 storage.reload()
-
-# /root/AirBnB_clone_v2/models/__init__.py
-
-from models.engine.db_storage import DBStorage
-
-storage = DBStorage()
-storage.reload()
