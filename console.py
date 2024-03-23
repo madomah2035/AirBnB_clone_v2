fd43547e9855f988516a5778d690d5c1c0cd4494
Musah ADOMAH <134120429+madomah2035@users.noreply.github.com>
2024-03-23 23:37:30 +0000

    fixed indentation error

--- console.py
+++ console.py
@@ -114,7 +114,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-    """ Create an object of any class with given parameters """
+        """ Create an object of any class with given parameters """
         try:
             if not args:
                 raise SyntaxError()
